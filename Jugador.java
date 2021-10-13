f9aefc456d78f85a4350f085a78747e9abf595b3
Tiki <thiagoqua16@gmail.com>
2021-10-13 11:55:21 -0300

    Inicio de desarrollo de función sistPuntuación()

--- Jugador.java
+++ Jugador.java
@@ -6,14 +6,19 @@ public class Jugador {
 	private String tag;					//nombre del jugador
 	private boolean soy_mano;
 	private boolean bandera;
+	private boolean cantoPrimi;
+	public Carta cartas[];
 	
-	/*MAS ADELANTE SE VERÁ SI EL NOMBRE SE INICIA DIRECTAMENTE EN EL CONSTRUCTOR, O LOS JUGADORES LO TIPEAN DESDE CONSOLA/INTERFAZ */
+	/*MAS ADELANTE SE VERA SI EL NOMBRE SE INICIA DIRECTAMENTE EN EL CONSTRUCTOR, O LOS JUGADORES LO TIPEAN DESDE CONSOLA/INTERFAZ */
 	
 	public Jugador(String tag) {
 		this.tag = tag;
 		this.puntos = 0;
 		this.soy_mano = false;
 		this.bandera = false;
+		this.cantoPrimi = false;	/* cuando se inicialize el arreglo de cantos, el jugador que cante primero inicializa
+		esta variable en true para poder sacar bien los puntos despuÃ©s */
+		this.cartas = new Carta[3];
 	}
 	
 	/* GETTERS */
@@ -22,17 +27,27 @@ public class Jugador {
 	public String getTag() {return tag;}
 	public boolean getMano() {return soy_mano;}
 	public boolean getBandera() {return bandera;}
+	public boolean getCantoPrimi() {return cantoPrimi;}
+	//public Carta[] getCartas() {return cartas;}
 	
 	/* SETTERS */
 	
-	public void setPuntos(int puntos) {this.puntos = puntos;}
 	public void setTag(String tag) {this.tag = tag;}
 	
 	/*Una primera forma para inicializar las banderas es directamente pasarle un argumento*/
-	/*Habrá que ver mas adelante cual es la forma mas efectiva*/
+	/*Habrï¿½ que ver mas adelante cual es la forma mas efectiva*/
 	
 	public void setBandera(boolean b) {this.bandera = b;}	
 	public void setMano(boolean m) {this.soy_mano = m;}	
+	public void setCantoPrimi(boolean m) {this.cantoPrimi = m;}
+	//public void setCartas(Carta nuevas[]) {this.cartas = nuevas;}
+
+	public void addPuntos(int nuevos){
+		if((this.puntos + nuevos) <= 30)
+			this.puntos+=nuevos;
+		else 
+			this.puntos = 30;
+	}
 	
 	/* POSIBLES FUNCIONES QUE SE LLEGUEN A NECESITAR */
 	/* REPRESENTAN TODAS LAS ACCIONES QUE PUEDE REALIZAR UN JUGADOR EN UNA RONDA */
@@ -53,5 +68,11 @@ public class Jugador {
 	public void quieroNoquiero() {
 	}	
 
+	public boolean envido(Jugador j2){
+		//se verifica quien gana el envido
+	return false;}
 	
+	public boolean truco(Jugador j2){
+		//se verifica quien gana el truco
+	return false;}
 }
--- Main.java
+++ Main.java
@@ -1,14 +1,10 @@
 package truco;
 public class Main{
- 
     public static void main(String[] args){
-    	
     	/* OBJETOS DE CLASES */
     	Jugador j1 = new Jugador("TIKI");
         Jugador j2 = new Jugador("ESTEBAN");
         Mazo m = new Mazo();
-    	Carta cartasj1 [] = new Carta[3];
-    	Carta cartasj2 [] = new Carta[3];	//arreglos que guardan las cartas de los jugadores (las que tendrian en sus manos)
         
         /* VARIABLES DE MAIN */
         int r = 0;	//reserva retorno de ronda() aunque no sirve pa' nada; lo pongo porque sino me tira error
@@ -18,7 +14,7 @@ public class Main{
         /*Podemos hacer que el jugador 1 siempre empiece por defecto cualquier partida*/
         /*Al menos yo cuando juego al truco entre 2 o 4, arranca cualquiera; no se hace sorteo ni nada*/
         
-        while(j1.getPuntos()!=30 || j2.getPuntos()!=30) {		//a partir de ac� se desarrolla toda la partida
+        while(j1.getPuntos()!=30 || j2.getPuntos()!=30) {		//a partir de ac� se desarrolla toda la partida
         	
         	if(i%2==0) {
                 j1.setBandera(true);	j1.setMano(false);
@@ -29,64 +25,85 @@ public class Main{
                 j2.setBandera(true);	j2.setMano(false);
         	}
         	
-        	r = ronda(j1, j2, m, cartasj1, cartasj2);		//todo lo que tenga que ver con la ronda se desarrolla en la funcion
+        	r = ronda(j1,j2,m,j1.cartas,j2.cartas);		//todo lo que tenga que ver con la ronda se desarrolla en la funcion
         	m.reponer();				//se reponen las cartas
         	i++;
-        }	
-        
-        /*
-        j1.setPuntos = sistPuntuacion(j1.getPuntos,1);
-        j2.setPuntos = sistPuntuacion(j2.getPuntos,10);
-        */
-        
-        
+        }
+        /*if(j1.getCantoPrimi())        //j1 en la funcion siempre va a ser el que canta primero
+            sistPuntuacion(cantos,j1,j2);
+        else
+            sistPuntuacion(cantos,j2,j1);*/
     }
-    
-    
-    public int[][] initPuntuacion(){
-        
-    	/* SE PUEDEN GUARDAR LOS PUNTOS EN UN ARREGLO, O SINO, EN VEZ DE CREAR MUCHAS VARIABLES, EL NOMBRE DE CADA VARIABLE...
-    	 * ...PUEDE SER UN STRING GUARDADO EN EL ARREGLO 
-    	 */
-    	
-    	/*CONSIDERAR CREAR UN ARREGLO PARA CADA TIPO: PARA LOS PUNTOS DE TRUCO, PARA LOS ENVIDOS Y PARA LOS NO QUERIDOS*/
-    	
-    	final int puntaje[] = new puntaje(4);
-        puntaje[] = {2,4,5,7};
-        final int e = 2;        //1
-        final int r = 3;        //2
-        final int ee = 4;       //3
-        final int er = 5;       //4
-        final int eer = 7;      //5
-
-        final int t = 2;        //6
-        final int ret = 3;      //7
-        final int cuat = 4;     //8
-
-        final int ne = 1;
-        final int nee = 2;
-        final int ner = 2;
-        final int neer = 4;
-        final int nr = 1;
-        final int nf = 1;
-        final int nef = 2;
-        final int neef = 4;
-        final int nerf = 5;
-        final int neerf = 7;
-        
-        final int nt = 1;
-        final int nret = 2;
-        final int nquat = 3;
-    }  
  
-    public int sistPuntuacion(int p,int q){
-        switch(q){
-            case 1:
-                return p - e;
-            case 2:
-                return p - ee;
-            case 10:
-                return 30 - p;
+    public void sistPuntuacion(String cantado[],Jugador j1,Jugador j2){
+        int index = 0;
+        if(cantado[index].equals("envido")){                    //canta j1
+            if(cantado[index++].equals("quiero"))               //canta j2
+                if(j1.envido(j2))
+                    j1.addPuntos(2);
+                else
+                    j2.addPuntos(2);
+            else if(cantado[index].equals("no quiero"))         //canta j2
+                j1.addPuntos(1);
+            else if(cantado[index].equals("envido"))            //canta j2
+                if(cantado[index++].equals("quiero"))           //canta j1
+                    if(j1.envido(j2))
+                        j1.addPuntos(4);
+                    else
+                        j2.addPuntos(4);
+                else if(cantado[index].equals("no quiero"))     //canta j1
+                    j2.addPuntos(1);
+                else if(cantado[index].equals("real envido"))   //canta j1
+                    if(cantado[index++].equals("quiero"))       //canta j2
+                        if(j1.envido(j2))
+                            j1.addPuntos(7);
+                        else
+                            j2.addPuntos(7);
+                    else if(cantado[index].equals("no quiero")) //canta j2
+                        j1.addPuntos(4);
+                    else if(cantado[index].equals("falta envido"))//canta j2
+                        if(cantado[index++].equals("quiero"))   //canta j1
+                            if(j1.envido(j2))
+                                j1.addPuntos(30);   //ver acá que pasa ocn la falta
+                            else
+                                j2.addPuntos(30);   //acá también
+                        else if(cantado[index].equals("no quiero"))//canta j1
+                            j2.addPuntos(7);
+                else if(cantado[index].equals("falta envido"))//canta j2
+                    if(cantado[index++].equals("quiero"))   //canta j1
+                        if(j1.envido(j2))
+                            j1.addPuntos(30);   //ver acá que pasa ocn la falta
+                        else
+                            j2.addPuntos(30);   //acá también
+                    else if(cantado[index].equals("no quiero"))//canta j1
+                        j2.addPuntos(7);
+            else if(cantado[index].equals("real envido"))   //canta j1
+                if(cantado[index++].equals("quiero"))       //canta j2
+                    if(j1.envido(j2))
+                        j1.addPuntos(7);
+                    else
+                        j2.addPuntos(7);
+                else if(cantado[index].equals("no quiero")) //canta j2
+                    j1.addPuntos(4);
+                else if(cantado[index].equals("falta envido"))//canta j2
+                    if(cantado[index++].equals("quiero"))   //canta j1
+                        if(j1.envido(j2))
+                            j1.addPuntos(30);   //ver acá que pasa ocn la falta
+                        else
+                            j2.addPuntos(30);   //acá también
+                    else if(cantado[index].equals("no quiero"))//canta j1
+                        j2.addPuntos(7);
+            else if(cantado[index].equals("falta envido"))//canta j2
+                if(cantado[index++].equals("quiero"))   //canta j1
+                    if(j1.envido(j2))
+                        j1.addPuntos(30);   //ver acá que pasa ocn la falta
+                    else
+                        j2.addPuntos(30);   //acá también
+                else if(cantado[index].equals("no quiero"))//canta j1
+                    j2.addPuntos(7);
+        }
+        else{               //se canto el truco
+
         }
     }
        
@@ -120,7 +137,7 @@ public class Main{
 			System.out.println(carta);
 		}
     	
-    	/*Se desarrolla la acci�n; empieza el que tiene la bandera en TRUE */
+    	/*Se desarrolla la acci�n; empieza el que tiene la bandera en TRUE */
     	
     	
     	//si quiero terminar una ronda utilizo returns en varias zonas del codigo, dependiendo lo que haya pasado
