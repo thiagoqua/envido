ea22577158bfe22fa3abf1b7f0e31b064106860c
Tiki <thiagoqua16@gmail.com>
2022-01-20 13:54:18 -0300

    Corrección de error en servidor.

--- EnvidoServer.java
+++ EnvidoServer.java
@@ -35,6 +35,7 @@ public class EnvidoServer{
 
     public void send(Object o){
         try{
+            listener = server.accept();
             dout = new DataOutputStream(listener.getOutputStream());
             dout.writeUTF(o.toString());
             dout.flush();
